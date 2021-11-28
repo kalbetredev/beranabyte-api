20e9afb3ce76c901c72450ab967053bb7df378f6
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-11-29 00:35:59 +0300

    add database abstract class

---
+++ api/database/database.py
@@ -0,0 +1,15 @@
+
+from abc import ABC, abstractmethod
+from typing import List
+from api.schemas.types.blog import Blog
+from api.schemas.types.user import User
+
+
+class Database(ABC):
+    @abstractmethod
+    def get_all_blogs(self) -> List[Blog]:
+        pass
+
+    @abstractmethod
+    def get_blog_author(self, blog_id: str) -> User:
+        pass
