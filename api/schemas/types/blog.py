76650cb00905e7874c5b9e53969286496c43084e
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-11-30 15:49:38 +0300

    add NewBlog type and make Blog type to inherit it

--- api/schemas/types/blog.py
+++ api/schemas/types/blog.py
@@ -1,24 +1,40 @@
 from __future__ import annotations
+from typing import Optional
 import strawberry
 from datetime import datetime
 from api.schemas.types.user import User
 from api import app
 
 
-@strawberry.type
-class Blog:
-    id: strawberry.ID
+@strawberry.input
+class NewBlog:
     title: str
     topic: str
-    is_featured: bool
-    is_published: bool
-    published_on: datetime
-    modified_on: datetime
     summary: str
     image_url: str
-    view_count: str
     content: str
+
+
+@strawberry.type
+class Blog(NewBlog):
+    id: strawberry.ID
+    is_featured: Optional[bool] = False
+    is_published: Optional[bool] = False
+    modified_on: Optional[datetime] = datetime.now()
+    view_count: Optional[int] = 0
+    published_on: Optional[datetime] = None
 
     @strawberry.field
     def author(root: Blog) -> User:
         return app.database.get_blog_author(root.id)
+
+    @classmethod
+    def fromNewBlog(cls, id: str, new_blog: NewBlog):
+        return Blog(
+            id=id,
+            title=new_blog.title,
+            topic=new_blog.topic,
+            summary=new_blog.summary,
+            image_url=new_blog.image_url,
+            content=new_blog.content,
+        )
