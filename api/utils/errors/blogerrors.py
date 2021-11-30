634dd4e3b0314c1924010d4ce203435514eae407
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-11-30 15:35:14 +0300

    add blog errors classes

---
+++ api/utils/errors/blogerrors.py
@@ -0,0 +1,23 @@
+import strawberry
+from typing import Optional
+from api.utils.errors.apierror import BaseAPIError, Error
+
+
+@strawberry.type
+class BlogCreationFailedError(BaseAPIError):
+    def __init__(self, message_detail: Optional[str] = ""):
+        self.message_detail = message_detail
+
+    @strawberry.field
+    def error(self) -> Error:
+        return Error(f"Error occurred trying to create your blog. {self.message_detail}".strip())
+
+
+@strawberry.type
+class BlogNotFoundError(BaseAPIError):
+    def __init__(self, blog_id: str):
+        self.blog_id = blog_id
+
+    @strawberry.field
+    def error(self) -> Error:
+        return Error(f"The blog with id {self.blog_id} could not be found")
