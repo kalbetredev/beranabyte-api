847000c95b09c184157492269c131d8cf13fc56d
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-12-01 08:31:53 +0300

    add a default message to api error

--- api/utils/errors/apierror.py
+++ api/utils/errors/apierror.py
@@ -1,5 +1,7 @@
 import strawberry
+from typing import Optional
 from abc import ABC, abstractmethod
+from api.utils.constants import messages
 
 
 @strawberry.type
@@ -16,6 +18,9 @@ class BaseError(ABC, BaseException):
 
 @strawberry.type
 class APIError(BaseError):
+    def __init__(self, message: Optional[str] = None) -> None:
+        self.message = message if message != None else messages.DEFAULT_ERROR_MESSAGE
+
     @strawberry.field
     def error(self) -> Error:
-        return Error("Error occurred trying to fullfill your request. Please try again.")
+        return Error(self.message)
