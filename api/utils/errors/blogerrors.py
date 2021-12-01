dcdd49ff8d0d82faa4ff90c4be8ca73aad1b71f7
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-12-01 08:33:17 +0300

    refactor blog errors to blog not found and blog title taken cases

--- api/utils/errors/blogerrors.py
+++ api/utils/errors/blogerrors.py
@@ -1,33 +1,17 @@
 import strawberry
-from typing import Optional
-from api.utils.errors.apierror import BaseAPIError, Error
+from api.utils.errors.apierror import APIError, BaseError, Error
+from api.utils.constants import messages
 
 
 @strawberry.type
-class BlogCreationFailedError(BaseAPIError):
-    def __init__(self, message_detail: Optional[str] = ""):
-        self.message_detail = message_detail
-
+class BlogNotFound(BaseError):
     @strawberry.field
     def error(self) -> Error:
-        return Error(f"Error occurred trying to create your blog. {self.message_detail}".strip())
+        return APIError(messages.BLOG_NOT_FOUND)
 
 
 @strawberry.type
-class BlogNotFoundError(BaseAPIError):
-    def __init__(self, blog_id: str):
-        self.blog_id = blog_id
-
+class BlogTitleTaken(BaseError):
     @strawberry.field
     def error(self) -> Error:
-        return Error(f"Blog with id {self.blog_id} could not be found")
-
-
-@strawberry.type
-class BlogUpdateFailedError(BaseAPIError):
-    def __init__(self, message_detail: Optional[str] = ""):
-        self.message_detail = message_detail
-
-    @strawberry.field
-    def error(self) -> Error:
-        return Error(f"Error occurred trying to update your blog. {self.message_detail}".strip())
+        return APIError(messages.BLOG_TITLE_TAKEN)
