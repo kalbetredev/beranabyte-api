95d1414e719533cf3fa9a4d7560d098cb136ddf3
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-12-04 14:42:01 +0300

    add register user database method

--- api/database/database.py
+++ api/database/database.py
@@ -2,7 +2,7 @@
 from abc import ABC, abstractmethod
 from typing import List, Union
 from api.schemas.types.blog import Blog, NewBlog, UpdatedBlog
-from api.schemas.types.user import User
+from api.schemas.types.user import User, UserAuth
 
 
 class Database(ABC):
@@ -52,4 +52,8 @@ class Database(ABC):
 
     @abstractmethod
     def get_user_by_id(self, user_id: str) -> Union[User, None]:
+        pass
+
+    @abstractmethod
+    def register_user(self, user: UserAuth) -> User:
         pass
