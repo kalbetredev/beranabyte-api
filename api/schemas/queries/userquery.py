4a7c730c6105129007d0ef60348147f0e0905bc1
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-12-04 12:39:48 +0300

    add user query

---
+++ api/schemas/queries/userquery.py
@@ -0,0 +1,21 @@
+import strawberry
+from api.schemas.types.user import User
+from api.utils.errors.usererrors import UserNotFound
+from api import app
+
+GetUserResponse = strawberry.union(
+    "GetUserResponse",
+    [User, UserNotFound]
+)
+
+
+@strawberry.type
+class UserQuery:
+    @strawberry.field
+    def user(self, user_id: str) -> GetUserResponse:
+        print(user_id)
+        user = app.database.get_user_by_id(user_id)
+        if user == None:
+            return UserNotFound()
+        else:
+            return user
