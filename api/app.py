074ce46d4b5a629ca32d8b96e5ff119fba9fe1f2
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-12-24 15:38:17 +0300

    add auth implementation

--- api/app.py
+++ api/app.py
@@ -1,15 +1,22 @@
-import uvicorn
+from typing import Optional
+
 import strawberry
-from fastapi import FastAPI
+import uvicorn
+from fastapi import Depends, FastAPI, Header, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security.oauth2 import OAuth2PasswordBearer
 from strawberry.fastapi import GraphQLRouter
+from tests.testdatabase import TestDatabase
+
+from api.auth.auth import Auth
+from api.auth.models.userauth import UserAuth
 from api.config.settings import settings
 from api.database.database import Database
-from api.schemas.queries.apiquery import Query
 from api.schemas.mutations.apimutation import Mutation
-from api.utils.logging.logger import Logger
+from api.schemas.queries.apiquery import Query
 from api.utils.logging.defaultlogger import DefaultLogger
-from tests.testdatabase import TestDatabase
+from api.utils.logging.logger import Logger
 
 schema = strawberry.Schema(query=Query, mutation=Mutation)
 
@@ -17,6 +24,7 @@ graphql_app = GraphQLRouter(schema, graphiql=settings.graphiql)
 
 database: Database = TestDatabase()
 logger: Logger = DefaultLogger()
+auth = Auth()
 
 app = FastAPI()
 app.include_router(graphql_app, prefix='/api')
@@ -32,6 +40,39 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*']
 )
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@app.post('/signup')
+async def signup(request: Request, form_data: OAuth2PasswordRequestForm = Depends(), user_agent: Optional[str] = Header(None)):
+    try:
+        user_auth = UserAuth(
+            email=form_data.username,
+            password=form_data.password,
+            user_agent=user_agent,
+            ip=request.client.host
+        )
+        response = await auth.signup(user_auth)
+        return response.get_json()
+    except Exception as error:
+        logger.error(__name__, error)
+
+
+@app.post('/signin')
+def signin(request: Request, form_data: OAuth2PasswordRequestForm = Depends(), user_agent: Optional[str] = Header(None)):
+    try:
+        user_auth = UserAuth(
+            email=form_data.username,
+            password=form_data.password,
+            user_agent=user_agent,
+            ip=request.client.host
+        )
+        response = auth.signin(user_auth)
+        return response.get_json()
+    except Exception as error:
+        logger.error(__name__, error)
 
 
 def start_app():
