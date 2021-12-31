44f185836f89cde4c7878c30fd38e0af607eaa83
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-12-31 13:03:01 +0300

    update token methods and custom error raising

--- api/auth/authdatabase.py
+++ api/auth/authdatabase.py
@@ -3,14 +3,17 @@ from logging import Logger
 import motor.motor_asyncio
 from api.auth.constants import (
     DEVICE_COLLECTION,
+    REFRESH_TOKENS_COLLECTION,
     SESSION_COLLECTION,
-    TOKEN_COLLECTION,
+    USER_TOKEN_COLLECTION,
 )
 from api.auth.models.device import Device
 from api.auth.models.session import Session
-from api.auth.models.token import Token
+from api.auth.models.token import UserToken
 from api.config.settings import settings
+from api.database.databaseerror import DatabaseError
 from api.utils.logging.defaultlogger import DefaultLogger
+from firebase_admin.credentials import RefreshToken
 
 
 class AuthDatabase:
@@ -21,7 +24,8 @@ class AuthDatabase:
         )
         self.auth_db = self.client[settings.auth_db_name]
         self.devices_collection = self.auth_db[DEVICE_COLLECTION]
-        self.tokens_collection = self.auth_db[TOKEN_COLLECTION]
+        self.refresh_token_collection = self.auth_db[REFRESH_TOKENS_COLLECTION]
+        self.user_tokens_collection = self.auth_db[USER_TOKEN_COLLECTION]
         self.sessions_collection = self.auth_db[SESSION_COLLECTION]
 
     async def add_device(self, device: Device) -> str:
@@ -30,17 +34,28 @@ class AuthDatabase:
             return result.inserted_id
         except Exception as error:
             self.logger.error(__name__, error)
+            raise DatabaseError("Unable to Save Device to Database")
 
-    async def add_token(self, token: Token) -> str:
+    async def add_refresh_token(self, token: RefreshToken) -> str:
         try:
-            result = await self.tokens_collection.insert_one(token.dict())
+            result = await self.refresh_token_collection.insert_one(token.dict())
             return result.inserted_id
         except Exception as error:
             self.logger.error(__name__, error)
+            raise DatabaseError("Unable to Save Refresh Token to Database")
 
-    async def add_session(self, session: Session) -> str:
+    async def add_user_token(self, token: UserToken) -> str:
+        try:
+            result = await self.user_tokens_collection.insert_one(token.dict())
+            return result.inserted_id
+        except Exception as error:
+            self.logger.error(__name__, error)
+            raise DatabaseError("Unable to Save User Token to Database")
+
+    async def add_session(self, session: Session) -> str | None:
         try:
             result = await self.sessions_collection.insert_one(session.dict())
             return result.inserted_id
         except Exception as error:
             self.logger.error(__name__, error)
+            raise DatabaseError("Unable to Save Session to Database")
