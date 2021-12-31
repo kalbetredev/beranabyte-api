fd114275730c64d58956e09ecc0448a2b4660c00
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-12-31 12:59:38 +0300

    refactor Token to TokenResponse

--- api/auth/models/token.py
+++ api/auth/models/token.py
@@ -1,24 +1,7 @@
-from datetime import datetime
-from typing import Optional
-
-from api.auth.models.userauthresponse import UserAuthResponse
 from pydantic import BaseModel
 
 
-class Token(BaseModel):
-    user_id: str
+class TokenResponse(BaseModel):
+    access_token: str
     refresh_token: str
-    issued_on: datetime
-    used_on: Optional[datetime] = None
-    is_revoked: bool
-
-    def __init__(self, user: UserAuthResponse):
-        super().__init__(
-            user_id=user.user_id,
-            refresh_token=user.refresh_token,
-            issued_on=datetime.now(),
-            is_revoked=False,
-        )
-
-    class Config:
-        allow_population_by_field_name = True
+    token_type: str = "bearer"
