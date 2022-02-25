a6777a1fb0369443fb131a7a9e89ca96c0dd97bb
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-02-26 00:07:51 +0300

    inherit from MongoModel for RefreshToken model

--- api/auth/models/refreshtoken.py
+++ api/auth/models/refreshtoken.py
@@ -3,13 +3,13 @@ from datetime import datetime
 from secrets import token_bytes
 from typing import Optional
 
-from pydantic.main import BaseModel
+from api.auth.models.mongomodel import MongoModel
 
 
-class RefreshToken(BaseModel):
+class RefreshToken(MongoModel):
     user_id: str
     created_by_ip: str
     value: Optional[str] = b64encode(token_bytes(64)).decode()
-    created_at: Optional[str] = datetime.utcnow()
-    replaced_by: Optional[str] = None
-    revoked_by_ip: Optional[str] = None
+    created_at: Optional[datetime] = datetime.utcnow()
+    replaced_by: Optional[datetime] = None
+    revoked_by_ip: Optional[datetime] = None
