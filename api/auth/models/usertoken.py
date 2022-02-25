99adc81f882454921ca20899bed8ca59ceecc7d4
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-02-26 00:07:18 +0300

    use PyObjectId on UserToken Model

--- api/auth/models/usertoken.py
+++ api/auth/models/usertoken.py
@@ -1,20 +1,24 @@
 from datetime import datetime
 from typing import Optional
 from api.auth.models.mongomodel import MongoModel
+from api.auth.models.pyobjectid import PyObjectId
 
 from api.auth.models.userauthresponse import UserAuthResponse
 
 
 class UserToken(MongoModel):
     user_id: str
-    api_refresh_token: str
+    api_refresh_token: PyObjectId
     firebase_refresh_token: str
     issued_on: datetime
     is_revoked: bool
     used_on: Optional[datetime] = None
 
     @staticmethod
-    def from_user_auth(user_auth: UserAuthResponse, api_refresh_token: str):
+    def from_user_auth(
+        user_auth: UserAuthResponse,
+        api_refresh_token: PyObjectId,
+    ):
         return UserToken(
             user_id=user_auth.user_id,
             api_refresh_token=api_refresh_token,
