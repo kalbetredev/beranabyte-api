02700a5744f1fc2d5eb2a101c91fae433ed56ba4
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-02-27 16:37:51 +0300

    bug fix for response json parsing in UserAuthResponse

--- api/auth/models/userauthresponse.py
+++ api/auth/models/userauthresponse.py
@@ -8,15 +8,16 @@ class UserAuthResponse(BaseModel):
 
     @classmethod
     def from_response(cls, response):
-        return UserAuthResponse(
-            user_id=response["localId"],
-            id_token=response["idToken"],
-            refresh_token=response["refreshToken"],
-        )
-
-    def get_json(self):
-        return {
-            "userId": self.user_id,
-            "idToken": self.id_token,
-            "refreshToken": self.refresh_token,
-        }
+        response_data = response.json()
+        if "localId" in response_data:
+            return UserAuthResponse(
+                user_id=response_data["localId"],
+                id_token=response_data["idToken"],
+                refresh_token=response_data["refreshToken"],
+            )
+        else:
+            return UserAuthResponse(
+                user_id=response_data["user_id"],
+                id_token=response_data["id_token"],
+                refresh_token=response_data["refresh_token"],
+            )
