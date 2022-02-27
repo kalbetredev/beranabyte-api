31ba7ee5901c1c0130bec110a1114fc2f72e66fb
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-02-27 16:28:13 +0300

    bug fix for generated token is not unique

--- api/auth/models/refreshtoken.py
+++ api/auth/models/refreshtoken.py
@@ -3,13 +3,19 @@ from datetime import datetime
 from secrets import token_bytes
 from typing import Optional
 
+from pydantic import Field
+
 from api.auth.models.mongomodel import MongoModel
+
+
+def generate_token():
+    return b64encode(token_bytes(64)).decode()
 
 
 class RefreshToken(MongoModel):
     user_id: str
     created_by_ip: str
-    value: Optional[str] = b64encode(token_bytes(64)).decode()
-    created_at: Optional[datetime] = datetime.utcnow()
-    replaced_by: Optional[datetime] = None
+    value: Optional[str] = Field(default_factory=generate_token)
+    created_at: Optional[datetime] = Field(default_factory=datetime.utcnow)
+    is_revoked: Optional[bool] = False
     revoked_by_ip: Optional[datetime] = None
