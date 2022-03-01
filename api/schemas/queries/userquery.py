3396f74ce45602566d248c791941cc1fea7a13f6
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-03-01 13:29:30 +0300

    add current user query

--- api/schemas/queries/userquery.py
+++ api/schemas/queries/userquery.py
@@ -1,9 +1,13 @@
 import strawberry
 from api.schemas.types.user import User
-from api.utils.errors.usererrors import UserNotFound
+from api.utils.errors.usererrors import UserNotAuthenticated, UserNotFound
 from api import app
+from strawberry.types import Info
 
 GetUserResponse = strawberry.union("GetUserResponse", [User, UserNotFound])
+GetCurrentUserResponse = strawberry.union(
+    "GetCurrentUserResponse", [User, UserNotAuthenticated]
+)
 
 
 @strawberry.type
@@ -15,3 +19,11 @@ class UserQuery:
             return UserNotFound()
         else:
             return user
+
+    @strawberry.field
+    def current_user(self, info: Info) -> GetCurrentUserResponse:
+        return (
+            User(info.context.user.uid, info.context.user.email)
+            if info.context.user is not None
+            else UserNotAuthenticated()
+        )
