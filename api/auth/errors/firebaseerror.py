b0a462524b28d7fde186400323784cebdadda5f2
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-03-01 13:28:34 +0300

    add INVALID_ID_TOKEN and USER_NOT_FOUND errors

--- api/auth/errors/firebaseerror.py
+++ api/auth/errors/firebaseerror.py
@@ -7,6 +7,8 @@ class FirebaseError(Enum):
     INVALID_PASSWORD = constants.INVALID_PASSWORD
     USER_DISABLED = constants.USER_DISABLED
     EMAIL_EXISTS = constants.EMAIL_EXISTS
+    INVALID_ID_TOKEN = constants.INVALID_ID_TOKEN
+    USER_NOT_FOUND = constants.USER_NOT_FOUND
     OTHER = 'OTHER'
 
     @staticmethod
@@ -20,5 +22,9 @@ class FirebaseError(Enum):
                 return FirebaseError.USER_DISABLED
             case constants.EMAIL_EXISTS:
                 return FirebaseError.EMAIL_EXISTS
+            case constants.INVALID_ID_TOKEN:
+                return FirebaseError.INVALID_ID_TOKEN
+            case constants.USER_NOT_FOUND:
+                return FirebaseError.USER_NOT_FOUND
             case _:
                 return FirebaseError.OTHER
