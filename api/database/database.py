9c16f006e6c64ae107bb3eb0e8b216378b0a7677
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-03-02 16:30:11 +0300

    update database abstract class methods

--- api/database/database.py
+++ api/database/database.py
@@ -1,50 +1,48 @@
 from abc import ABC, abstractmethod
 from typing import List, Union
 from api.schemas.types.blog import Blog, NewBlog, UpdatedBlog
-from api.schemas.types.user import User
+from api.schemas.types.user import User, UserMeta
 
 
 class Database(ABC):
     @abstractmethod
-    def get_all_blogs(self) -> List[Blog]:
+    async def get_blogs(
+        self, user_id: str | None = None, is_published: bool | None = None
+    ) -> List[Blog]:
         pass
 
     @abstractmethod
-    def get_blogs(self, is_published: bool) -> List[Blog]:
+    async def get_blog(self, blog_id: str) -> Union[Blog, None]:
         pass
 
     @abstractmethod
-    def get_blog_by_id(self, blog_id: str) -> Union[Blog, None]:
+    async def get_all_topics(self) -> List[str]:
         pass
 
     @abstractmethod
-    def get_blog_author(self, blog_id: str) -> Union[User, None]:
+    async def create_new_blog(self, new_blog: NewBlog) -> Blog:
         pass
 
     @abstractmethod
-    def get_user_blogs(self, user_id: str) -> List[Blog]:
+    async def update_blog(self, updated_blog: UpdatedBlog) -> Blog:
         pass
 
     @abstractmethod
-    def get_all_topics(self) -> List[str]:
+    async def delete_blog(self, blog_id: str):
         pass
 
     @abstractmethod
-    def create_new_blog(self, new_blog: NewBlog) -> Blog:
+    async def publish_blog(self, blog_id: str):
         pass
 
     @abstractmethod
-    def update_blog(self, updated_blog: UpdatedBlog) -> Blog:
+    async def increment_blog_view_count(self, blog_id: str):
         pass
 
     @abstractmethod
-    def delete_blog(self, blog_id: str):
+    async def add_user_meta(self, user_meta: UserMeta) -> bool:
         pass
 
     @abstractmethod
-    def publish_blog(self, blog_id: str):
-        pass
-
-    @abstractmethod
-    def increment_blog_view_count(self, blog_id: str):
+    async def get_user_meta(self, user_id: str) -> UserMeta:
         pass
