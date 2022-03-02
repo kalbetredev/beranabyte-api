7010ade426802135ca7d4a4dfd90f0d38911dd68
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-03-02 16:34:21 +0300

    add UserRole UserBase and UserMeta models

--- api/schemas/types/user.py
+++ api/schemas/types/user.py
@@ -1,7 +1,27 @@
 import strawberry
+from enum import Enum
+from typing import Optional
+
+
+class UserRole(Enum):
+    ADMIN = "admin"
+    AUTHOR = "author"
+    USER = "user"
+
+
+@strawberry.interface
+class UserBase:
+    user_id: strawberry.ID
+    email: str
 
 
 @strawberry.type
-class User:
-    id: strawberry.ID
-    email: str
+class UserMeta:
+    user_id: strawberry.ID
+    role: str
+    photo_url: Optional[str] = None
+
+
+@strawberry.type
+class User(UserMeta, UserBase):
+    pass
