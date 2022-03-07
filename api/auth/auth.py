356697ce320da83449286a6cc46b622af277bb0a
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-03-07 07:47:30 +0300

    change UserMeta to UserModel

--- api/auth/auth.py
+++ api/auth/auth.py
@@ -8,8 +8,8 @@ from api.auth.models.token import TokenResponse
 from api.auth.models.userauth import UserAuth
 from api.auth.models.userauthresponse import UserAuthResponse
 from api.auth.models.usertoken import UserToken
+from api.database.models.user_model import UserModel, UserRole
 from api.database.mongo_database import MongoDatabase
-from api.schemas.types.user import UserMeta, UserRole
 from api.utils.constants.messages import (
     SIGNIN_FAILED,
     SIGNUP_FAILED,
@@ -34,10 +34,12 @@ class Auth:
                 user_auth.email,
                 user_auth.password,
             )
-            user_meta = UserMeta(
-                user_id=response.user_id, role=UserRole.USER.value, photo_url=""
+            user_data = UserModel(
+                user_id=response.user_id,
+                role=UserRole.USER.value,
+                photo_url="",
             )
-            await self.main_db.add_user_meta(user_meta)
+            await self.main_db.add_user(user_data)
             return await self.generate_user_token(
                 response=response,
                 user_ip=user_auth.ip,
