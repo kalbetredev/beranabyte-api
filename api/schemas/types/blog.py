33ab818a5c72520d01004b25175202d21bdc8274
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-03-07 11:22:26 +0300

    update NewBlog and UpdatedBlog types

--- api/schemas/types/blog.py
+++ api/schemas/types/blog.py
@@ -12,16 +12,20 @@ class Blog:
 @strawberry.input
 class NewBlog:
     title: str
-    topic: Optional[str] = None
-    summary: Optional[str] = None
-    image_url: Optional[str] = None
-    content: Optional[str] = None
+    topic: str
+    summary: Optional[str] = ""
+    image_url: Optional[str] = ""
+    content: Optional[str] = ""
     is_featured: Optional[bool] = False
 
 
 @strawberry.input
-class UpdatedBlog(NewBlog):
+class UpdatedBlog:
     title: Optional[str] = None
+    topic: Optional[str] = None
+    summary: Optional[str] = None
+    image_url: Optional[str] = None
+    content: Optional[str] = None
     is_published: Optional[bool] = None
 
 
