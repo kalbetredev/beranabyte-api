f747e78a13e78b3cea8aa3f3c76279011c1a3234
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-03-07 07:57:28 +0300

    update method implmentations with pydantic models usage

--- api/schemas/queries/userquery.py
+++ api/schemas/queries/userquery.py
@@ -2,12 +2,13 @@ import strawberry
 from api.auth.firebaseadmin import get_user_record
 from api.database.database import Database
 from api.schemas.types.user import User
+from api.utils.errors.apierror import APIError
 from api.utils.errors.usererrors import UserNotAuthenticated, UserNotFound
 from strawberry.types import Info
 
-GetUserResponse = strawberry.union("GetUserResponse", [User, UserNotFound])
+GetUserResponse = strawberry.union("GetUserResponse", [User, UserNotFound, APIError])
 GetCurrentUserResponse = strawberry.union(
-    "GetCurrentUserResponse", [User, UserNotAuthenticated]
+    "GetCurrentUserResponse", [User, UserNotAuthenticated, APIError]
 )
 
 
@@ -15,25 +16,41 @@ GetCurrentUserResponse = strawberry.union(
 class UserQuery:
     @strawberry.field
     async def user(self, user_id: str, info: Info) -> GetUserResponse:
-        db: Database = info.context.db
-        user_record = get_user_record(user_id)
-        if user_record == None:
-            return UserNotFound()
-        else:
-            user_meta = await db.get_user_meta(user_record.uid)
-            return User(
-                email=user_record.email,
-                **user_meta.__dict__,
-            )
+        try:
+            db: Database = info.context.db
+            user_record = get_user_record(user_id)
+            user_data = await db.get_user(user_id)
+
+            if user_record is None or user_data is None:
+                return UserNotFound()
+            else:
+                data = user_data.dict()
+                del data["id"]
+                return User(
+                    email=user_record.email,
+                    **data,
+                )
+        except Exception as error:
+            info.context.logger.error(__name__, error)
+            return APIError()
 
     @strawberry.field
     async def current_user(self, info: Info) -> GetCurrentUserResponse:
-        db: Database = info.context.db
-        if info.context.current_user is not None:
-            user_meta = await db.get_user_meta(info.context.current_user.uid)
-            return User(
-                email=info.context.current_user.email,
-                **user_meta.__dict__,
-            )
-        else:
-            return UserNotAuthenticated()
+        try:
+            if info.context.current_user is not None:
+                db: Database = info.context.db
+                user_id = info.context.current_user.uid
+                email = info.context.current_user.email
+                user_data = await db.get_user(user_id)
+
+                if user_data is None:
+                    return UserNotAuthenticated()
+                else:
+                    data = user_data.dict()
+                    del data["id"]
+                    return User(email=email, **data)
+            else:
+                return UserNotAuthenticated()
+        except Exception as error:
+            info.context.logger.error(__name__, error)
+            return APIError()
