3129b710a72c625d09ad89e09ed12f3310f34bae
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-03-07 11:48:00 +0300

    use validation error for add new blog mutation

--- api/schemas/mutations/blogmutation.py
+++ api/schemas/mutations/blogmutation.py
@@ -5,13 +5,10 @@ from api.database.database import Database
 from api.database.models.blog_model import BlogModel
 from api.schemas.types.blog import Blog, NewBlog, UpdatedBlog
 from api.utils.errors.apierror import APIError
-from api.utils.errors.blogerrors import (
-    BlogNotFound,
-    BlogTitleCanNotBeEmpty,
-    BlogTitleTaken,
-)
+from api.utils.errors.blogerrors import BlogNotFound
 from api.schemas.types.responses import Success
 from api.utils.constants import messages
+from api.utils.errors.validationerror import InputError, InputValidationError
 
 
 ModifyBlogResult = strawberry.union(
@@ -29,14 +26,23 @@ class BlogMutation:
     @strawberry.mutation
     async def add_new_blog(
         self, new_blog: NewBlog, info: Info
-    ) -> Union[Blog, BlogTitleTaken, BlogTitleCanNotBeEmpty, APIError]:
+    ) -> Union[Blog, InputValidationError, APIError]:
         try:
             db: Database = info.context.db
+
             if new_blog.title == "":
-                return BlogTitleCanNotBeEmpty()
+                title_empty = InputError(
+                    input="title", message=messages.BLOG_TITLE_EMPTY
+                )
+                return InputValidationError([title_empty])
+
             existing_blog = await db.get_blog_by_title(new_blog.title)
             if existing_blog is not None:
-                return BlogTitleTaken()
+                title_taken = InputError(
+                    input="title", message=messages.BLOG_TITLE_TAKEN
+                )
+                return InputValidationError([title_taken])
+
             blog = BlogModel(**new_blog.__dict__)
             saved_blog = await db.add_new_blog(blog)
             return Blog(**saved_blog.dict()) if saved_blog is not None else APIError()
