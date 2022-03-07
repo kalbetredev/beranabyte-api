611d7d037859b833ce6e8ef62878e4bee7534b64
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-03-07 07:50:06 +0300

    refactor Blog type to use BlogBase pydantic model

--- api/schemas/types/blog.py
+++ api/schemas/types/blog.py
@@ -1,24 +1,12 @@
-from __future__ import annotations
 import strawberry
 from typing import Optional
-from datetime import datetime
-from api.schemas.types.user import User
+
+from api.database.models.blog_model import BlogBase
 
 
-@strawberry.type
+@strawberry.experimental.pydantic.type(model=BlogBase, all_fields=True)
 class Blog:
     id: str
-    title: str
-    topic: Optional[str] = None
-    summary: Optional[str] = None
-    image_url: Optional[str] = None
-    content: Optional[str] = None
-    is_featured: Optional[bool] = False
-    is_published: Optional[bool] = False
-    modified_on: Optional[datetime] = datetime.now()
-    view_count: Optional[int] = 0
-    published_on: Optional[datetime] = None
-    author: Optional[User] = None
 
 
 @strawberry.input
