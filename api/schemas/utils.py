deec49096953a991f85b896f35f1f0dee6651480
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-03-08 15:10:15 +0300

    add utility function to validate and check image

--- api/schemas/utils.py
+++ api/schemas/utils.py
@@ -1,3 +1,5 @@
+import imghdr
+import os
 from strawberry.types import Info
 from api.database.database import Database
 from api.database.models.user_model import UserModel, UserRole
@@ -17,3 +19,15 @@ def update_attributes(updated, existing) -> None:
     for attr, value in updated.__dict__.items():
         if value:
             setattr(existing, attr, value)
+
+
+def validate_image_type(file) -> bool:
+    file_type = imghdr.what(file)
+    return file_type in ["png", "jpeg", "gif", "bmp", "webp"]
+
+
+async def check_file_size(file, max_allowed) -> bool:
+    await file.seek(0, os.SEEK_END)
+    file_size = file.tell() / 1000000
+    await file.seek(0)
+    return file_size < max_allowed
