edea6101d16aa6e1bc98f766e85934ce6cf21b03
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-03-18 22:32:10 +0300

    add ProjectQuery to api Query

--- api/schemas/queries/apiquery.py
+++ api/schemas/queries/apiquery.py
@@ -1,8 +1,9 @@
 import strawberry
 from api.schemas.queries.blogquery import BlogQuery
+from api.schemas.queries.projectquery import ProjectQuery
 from api.schemas.queries.userquery import UserQuery
 
 
 @strawberry.type
-class Query(BlogQuery, UserQuery):
+class Query(BlogQuery, UserQuery, ProjectQuery):
     pass
