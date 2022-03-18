4165324607935fade9618e8620cf335fe2c68938
Kalkidan Betre <kalbetre.dev@outlook.com>
2022-03-18 17:35:24 +0300

    add update_project mutation

--- api/schemas/mutations/projectmutation.py
+++ api/schemas/mutations/projectmutation.py
@@ -50,3 +50,53 @@ class ProjectMutation:
             info.context.logger.error(__name__, error)
             return APIError()
 
+    @strawberry.mutation
+    async def update_project(
+        self, id: str, project: UpdatedProject, info: Info
+    ) -> Union[Project, ProjectNotFound, InputValidationError, APIError]:
+        try:
+            db: Database = info.context.db
+
+            if not await utils.is_current_user_admin(info):
+                return APIError(messages.UNAUTHORIZED_ACCESS)
+
+            if not ObjectId.is_valid(id):
+                return InputValidationError(
+                    [InputError(input="id", message=messages.INVALID_ID)]
+                )
+            else:
+                existing_project = await db.get_project_by_id(id)
+                if existing_project is None:
+                    return ProjectNotFound()
+
+                validation_errors: List[
+                    InputError
+                ] = projectvalidators.validate_project_update_inputs(project)
+
+                if len(validation_errors) > 0:
+                    return InputValidationError(validation_errors)
+
+                if (
+                    existing_project.title != project.title
+                    and await db.get_project_by_title(project.title) is not None
+                ):
+                    return InputValidationError(
+                        [
+                            InputError(
+                                input="title", message=messages.PROJECT_TITLE_TAKEN
+                            )
+                        ]
+                    )
+
+                utils.update_attributes(updated=project, existing=existing_project)
+                saved_project = await db.update_project(existing_project)
+                return (
+                    Project(**saved_project.dict())
+                    if saved_project is not None
+                    else APIError()
+                )
+
+        except Exception as error:
+            info.context.logger.error(__name__, error)
+            return APIError()
+
